cd329d4089e563348ae61a2f415860154533cdd8
Michael Fry-White <michaelfry2002@gmail.com>
2017-02-05 19:08:04 +0000

    Fix

--- samples/QueryableStrings.Samples.EntityFrameworkCore/Controllers/EmployeeApiController.cs
+++ samples/QueryableStrings.Samples.EntityFrameworkCore/Controllers/EmployeeApiController.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using NthCommit.AspNetCore.Mvc.QueryableStrings.Filtering;
 
 namespace QueryableStrings.EntityFrameworkCore.Controllers
 {
